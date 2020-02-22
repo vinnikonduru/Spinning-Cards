04153ecdc116336c2ce523f398f7410efd22a434
vinnikonduru <vinithakonduru@gmail.com>
2020-02-21 18:09:13 -0600

    Updates

---
+++ precache-manifest.4cf18d6d2a57d75192db7d1f37b8a2dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a68935c6f8f8a97bc67b29f7728a20",
+    "url": "/Spinning-Cards/index.html"
+  },
+  {
+    "revision": "333c3bd425d652c8923d",
+    "url": "/Spinning-Cards/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3523f99121ef5091371e",
+    "url": "/Spinning-Cards/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "333c3bd425d652c8923d",
+    "url": "/Spinning-Cards/static/js/2.a51c31b5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Spinning-Cards/static/js/2.a51c31b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3523f99121ef5091371e",
+    "url": "/Spinning-Cards/static/js/main.b9c8e61a.chunk.js"
+  },
+  {
+    "revision": "a0d57adda3759171546e",
+    "url": "/Spinning-Cards/static/js/runtime-main.4c0a4520.js"
+  }
+]);
